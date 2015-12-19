9f6aede5873b217d95081a25dd0385ae2ddcd5e6
bjorn <tie.372@gmail.com>
2015-12-18 22:39:45 -0800

    Add package.lua;

---
+++ package.lua
@@ -0,0 +1,8 @@
+return {
+  name = 'bjornbytes/graphql',
+  version = '0.0.1',
+  description = 'Lua GraphQL implementation',
+  license = 'MIT',
+  author = { url = 'https://github.com/bjornbytes' },
+  homepage = 'https://github.com/bjornbytes/graphql-lua'
+}

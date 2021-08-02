92d6b2834d8d740ec8af4118414fcb2fa8d5b1e6
Christian Kurz <chrikuvellberg@gmail.com>
2021-08-02 15:13:34 +0200

    always send interfaces to introspection

--- graphql/introspection.lua
+++ graphql/introspection.lua
@@ -230,7 +230,7 @@ __Type = types.object({
         kind = types.list(types.nonNull(__Type)),
         resolve = function(kind)
           if kind.__type == 'Object' then
-            return kind.interfaces
+            return kind.interfaces or {}
           end
         end
       },
